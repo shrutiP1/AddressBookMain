8ed6eeefb71d36d973f7c3b6471ac9488cb6be3a
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-14 08:45:24 +0530

    Welcome To Address Book.

---
+++ src/main/java/com/bridgelab/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelab.addressbook;
+
+public class AddressBookMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book ");
+
+    }
+}
